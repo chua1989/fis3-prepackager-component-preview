9c9b51efabf7c784dc242ea1ccefa4105611925a
chua1989 <chen_hua89@163.com>
2016-09-22 21:42:55 +0800

    出版完成

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # fis3-prepackager-component-preview
 fis3组件可视化插件
+
+## 配置方式(每个配置选项都是必须的)
+
+```js
+fis.match('::package', {
+    prepackager: fis.plugin('component-preview',{
+        wrap: '/v_components/wrap.html',//组件可视化原型文件，用来包裹组件可视化代码
+        url: '/v_components.html', //目标文件
+        COMPath: '/common/module',//组件集合目录
+        moduleListInstead: 'instead of modules',//使用模块列表节点替换当前文本
+        moduleViewInstead: 'instead of view htmls',//使用模块视图列表节点替换当前文本
+        moduleCommentsInstead: 'instead of commnets',//使用模块注释列表节点替换当前文本
+        moduleJsInstead: 'instead of js'//使用js脚本节点替换当前文本
+    })
+})
+```
+v_components.html为组件可视化生成的文件。最终在浏览器中打开该文件即可看到组件化效果
+
+wrap.html是用来包裹组件可视化代码的。需要的js、css需要自己去配置
+
+## 注意：
+
+组件文件中"@example"和"@example end"之间的字符串被认为是代码段：
+	1）不要出现不符合代码格式的字符
+	2)"html:"、"js:"分别为html代码段和js代码段开始的标志。其后的代码分别要严格按照html和js的格式要求书写
+
+
+匹配demo代码段的正则为/\/\*\*([\s\S]*)@example[\s\S]*?html:([\s\S]*?)js:([\s\S]*?)@example end([\s|\S]*?)\*\//
--- index.js
+++ index.js
@@ -1,17 +1,111 @@
 /**
 要求组件js的名称即为组件名称。
 settings = {
-	wrap: 'xx/xx..html',//组件可视化原型文件，用来包裹组件可视化代码
-	COMPath: 'xx/xxx',//组件集合目录
-	COMListClass: 'xxx',//组件列样式class名称
-	COMViewClass: 'xxx',//组件展示框class名称
-	COMDetailClass: 'xxx', //组件详情展示框class名称
+	wrap: '/xx/xx..html',//组件可视化原型文件，用来包裹组件可视化代码
+	url: '/xx/xx.html', //组件可视化最终的目标文件
+	COMPath: '/xx/xxx',//组件集合目录
+	moduleListInstead: 'xxx',//使用模块列表节点替换当前文本
+	moduleViewInstead: 'xx',//使用模块视图列表节点替换当前文本
+	moduleCommentsInstead: 'xxx',//使用模块注释列表节点替换当前文本
+	moduleJsInstead: 'xxx',//使用js脚本节点替换当前文本
 }
 demo代码匹配规则'example': /\/\*\*([\s\S]*)@example[\s\S]*?html:([\s\S]*?)js:([\s\S]*?)@example end([\s|\S]*?)\*\//
+// ret.src 所有的源码，结构是 {'<subpath>': <File 对象>},包括所有的文件
+// ret.ids 所有源码列表，结构是 {'<id>': <File 对象>},只包含资源文件如css,js,tpl,json等，不包括html、图片等文件
+// ret.map 包含两个属性：res和pkg。如果是 spriter、postpackager 这时候已经能得到打包结果了，可以修改静态资源列表或者其他
+// ret.pkg
 */
-module.exports = function(ret, pack, settings, opt) {
-	var src = ret.src;
-	Object.keys(src).forEach(function(key){
-		fis.log.notice(key + ' : ' + src[key]);
-	})	
+var path = require('path');
+var regs = {
+		//懒惰匹配到第一个*/
+		'example': /\/\*\*([\s\S]*)@example[\s\S]*?(html:([\s\S]*?)js:([\s\S]*?))@example end([\s|\S]*?)\*\//,//懒惰匹配第一个*/
+		'jsfile': /\S*(\/(\S+)*?\.js)$/,
+		'modName': /\/(\S+\.js)/
+	},
+	innerLeft = '',
+	innerRightT = '',
+	innerRightB = '',
+	innerJs = '';
+	
+//将str字符串转换成HTML格式
+function transToHtml(str){
+	var tran = [/&/g, />/g, /</g, /\n/g, / /g],//先要处理'&'才能处理其他标签，否则其他标签生成的‘&’会被处理
+		to = ['&amp;', '&gt;', '&lt;', '<br>', '&nbsp;'];
+	for(var i = 0; i < tran.length; i++){
+		str = str.replace(tran[i], to[i]);
+	}
+	return str;
 }
+module.exports = function(ret, pack, settings, opt) {
+	var src = ret.src,
+		com = {},
+		root = fis.project.getProjectPath(),
+		wrap,//包裹文件
+		newFile = fis.file.wrap(path.join(root, settings.url));//新文件
+	//fis.log.notice('settings.COMPath : ' + settings.COMPath);	
+
+	Object.keys(src).forEach(function(key){
+		if(RegExp(settings.COMPath).test(key) && /\.js$/.test(key)){
+			com[key] = src[key];
+		}
+
+		if(RegExp(settings.wrap).test(key)){
+			wrap = fis.file.wrap(src[key]);
+		}
+	});
+	if(!wrap){
+		console.log("wrap file path is not correct! wrap:" + wrap);
+	}
+
+	var requires = [];
+	//遍历所有模块文件
+	for(var buf in com){
+		var match,
+			moduleName = buf.match(regs.jsfile)[2];//["/xxx/ddd.js", "/ddd.js", "ddd"]
+		if(match = com[buf].getContent().match(regs.example)){
+			var comments = match[0].replace(regs.example,'$1$4')
+				.replace(/((\r|\n)\s*)(\*)/g, '$1')
+				.replace(regs.enter, '\n');
+
+			innerLeft += '<div data-mod="'+ moduleName +'">' + moduleName + '</div>';
+			innerRightT += '<div data-mod="'+ moduleName +'">' + match[3] + '</div>';
+			innerRightB += '<div data-mod="'+ moduleName +'">' 
+				+ '<div >样例：<div>' + transToHtml(match[2].replace(regs.enter, '\n')) +'</div></div>'
+				//去掉代码用例区域，去掉每一行之前的*符号
+				+ '<div >其他：<div>' + transToHtml(comments) + '</div></div></div>';
+			innerJs += match[4] + ";";
+
+			//添加依赖
+			var modN = buf.replace(regs.modName, '$1');
+			//fis.log.notice('com[' + buf + '].requires:' + com[buf].requires)
+			requires = requires.concat(modN);
+			requires = requires.concat(com[buf].requires);
+		}
+	}
+
+	//新加的组件可视化页面
+	var content = wrap.getContent();
+	// fis.log.notice('settings.moduleListInstead:' + settings.moduleListInstead);
+	// fis.log.notice('innerLeft:' + innerLeft);
+	// fis.log.notice(RegExp(settings.moduleListInstead).test(content.toString()));
+
+	//最终写入
+	content = content.replace(settings.moduleListInstead, innerLeft)
+		.replace(settings.moduleViewInstead, innerRightT)
+		.replace(settings.moduleCommentsInstead, innerRightB)
+		.replace(settings.moduleJsInstead, innerJs);
+
+	    // 派送事件
+    var message = {
+      file: wrap,
+      content: content,
+      newFile: newFile
+    };
+    fis.emit('pack:file', message);
+
+	newFile.setContent(content);
+	newFile.requires = requires;
+	ret.src[newFile.subpath] = newFile;
+	fis.compile(newFile);
+}
+
